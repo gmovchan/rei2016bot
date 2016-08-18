85fd4f42ec90c93b1bf87845143ec6b6922a8963
grigoryMovchan <grigory08@ya.ru>
2016-08-18 18:59:36 +0300

    make it better

--- robot.php
+++ robot.php
@@ -100,10 +100,12 @@ function processMessage($message) {
     // incoming text message
     $text = $message['text'];
     if (strpos($text, "/старт") === 0) {
-      apiRequestJson("sendMessage", array('chat_id' => $chat_id, "text" => 'Привет', 'reply_markup' => array(
+      apiRequestJson("sendMessage", array('chat_id' => $chat_id, "text" => 'Привет'
+      /*, 'reply_markup' => array(
         'keyboard' => array(array('Привет', "/отношения", "/сходка")),
         'one_time_keyboard' => false,
-        'resize_keyboard' => false)));
+        'resize_keyboard' => false)*/
+      ));
     } else if ($text === "Привет") {
       apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => 'Приятно с вами познакомиться'));
     } else if ($text === "/сходка") {

15a9806b09a421344cc9472f0b2e09f56257d901
grigoryMovchan <grigory08@ya.ru>
2016-08-20 13:23:25 +0300

    make it better

--- robot.php
+++ robot.php
@@ -111,7 +111,7 @@ function processMessage($message) {
     } else if (strpos($text, "/stop") === 0) {
       // stop now
     } else if (strpos($text, "/help") === 0) {
-      apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => 'Правила конфы, нарушение которых карается изгнанием: \n 1. Постинг детской порнографии \n 2. Вайп'));
+      apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => "Правила конфы, нарушение которых карается изгнанием: \n 1. Постинг детской порнографии \n 2. Вайп"));
     } else {
       apiRequestWebhook("sendMessage", array('chat_id' => $chat_id, "reply_to_message_id" => $message_id, "text" => 'Продолжай'));
     }

0bac098bb6b8dee259488f14d45f8e9adcb50700
grigoryMovchan <grigory08@ya.ru>
2016-08-22 22:53:02 +0300

    make it better

--- robot.php
+++ robot.php
@@ -40,8 +40,12 @@ $response = curl_exec($handle);
 */
 //  $parameters = array('chat_id' => $chat_id, "text" => 'Птичка');
 //  $url = API_URL.'sendMessage?'.http_build_query($parameters);
+  $dir    = '/img';
+  $files1 = scandir($dir);
+  $files1 = implode($files1);
   $randomArr = $arrayName = array('a' => '1', 'b' => '2');
-  $url = API_URL.'sendMessage?chat_id='.$chat_id.'&text='.implode($randomArr);
+  $randomArr = implode($randomArr);
+  $url = API_URL.'sendMessage?chat_id='.$chat_id.'&text='.$files1;
   $handle = curl_init($url);
   curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
   curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 5);
